24606b9b64688361fa88463eb55e45cb56408395
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2022-09-14 05:31:15 +0000

    Start mergers
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 	"os/exec"
 	"path/filepath"
+	"sync"
+
+	spdx22JSON "sigs.k8s.io/bom/pkg/spdx/json/v2.2.2"
 )
 
 const (
 	SyftImageURL  string = "docker.io/anchore/syft"
-	K8BomImageURL string = "k8s.gcr.io/bom/bom:v0.3.0"
-	MSBomURL      string = "ghcr.io/sbs2001/ms_sbom:v0.0.1"
+	K8BomImageURL string = "docker.io/sbs2001/k8s_bom:latest"
+	MSBomURL      string = "ghcr.io/sbs2001/ms_sbom:v0.0.2"
+	SpdxImageURL  string = "docker.io/spdx/spdx-sbom-generator"
 )
+
+// var SpdxToolSlugToPurlType = map[string]string{
+// 	"cargo":      "cargo",
+// 	"composer":   "composer",
+// 	"bundler":    "gem",
+// 	"go-mod":     "golang",
+// 	"Java-Maven": "maven",
+// 	"npm":        "npm",
+// 	"nuget":      "nuget",
+// 	"pipenv":     "pypi",
+// 	"poetry":     "pypi",
+// 	"pyenv":      "pypi",
+// 	"swift":      "swift",
+// 	"yarn":       "npm",
+// }
+
+// func ecosystemFromSpdxFileName(fileName string) string {
+// 	parts := strings.Split(fileName, ".")
+// 	return SpdxToolSlugToPurlType[parts[0][4:]]
+// }
 
 func main() {
 	dirToScan := flag.String("s", "./", "directory to scan")
@@ -21,11 +47,125 @@ func main() {
 		panic(err)
 	}
 
-	// z := []string{"docker", "run", "-v", K8BomImageURL, fullPath + ":" + "/scan", "generate", "/scan"}
-	// fmt.Println(strings.Join(z, " "))
-	out, err := exec.Command("docker", "run", "-v", fullPath+":"+"/scan", K8BomImageURL, "generate", "/scan").CombinedOutput()
+	scanResultsByTool := make(map[string]spdx22JSON.Document)
+
+	wg := sync.WaitGroup{}
+	wg.Add(4)
+	// microsoft bom generator
+	go func() {
+		defer wg.Done()
+		tmpDir, err := os.MkdirTemp(os.TempDir(), "ubom")
+		if err != nil {
+			panic(tmpDir)
+		}
+		_, err = exec.Command("docker", "run", "-t",
+			"-v", fullPath+":"+"/scan",
+			"-v", tmpDir+":"+tmpDir,
+			MSBomURL, "generate", "-b", "/scan",
+			"-bc", "/scan", "-pn", "blah",
+			"-nsb", "https://companyName.com/teamName",
+			"-pv", "0.0.1", "-D", "true", "-V", "Fatal",
+			"-m", tmpDir,
+		).CombinedOutput()
+
+		if err != nil {
+			panic(err)
+		}
+		manifestPath := filepath.Join(tmpDir, "_manifest", "spdx_2.2", "manifest.spdx.json")
+		data, err := os.ReadFile(manifestPath)
+		if err != nil {
+			panic(err)
+		}
+		t := spdx22JSON.Document{}
+		if err := json.Unmarshal(data, &t); err != nil {
+			panic(err)
+		}
+		scanResultsByTool["msbom"] = t
+	}()
+
+	go func() {
+		defer wg.Done()
+		tmpDir, err := os.MkdirTemp(os.TempDir(), "ubom")
+		if err != nil {
+			panic(tmpDir)
+		}
+		_, err = exec.Command(
+			"docker", "run", "-v", fullPath+":"+"/scan",
+			"-v", tmpDir+":"+tmpDir,
+			SpdxImageURL, "-p", "/scan",
+			"-o", tmpDir,
+			"--format", "json",
+		).CombinedOutput()
+
+		if err != nil {
+			panic(err)
+		}
+
+		entries, err := os.ReadDir(tmpDir)
+		if err != nil {
+			panic(err)
+		}
+		merged := spdx22JSON.Document{}
+		for _, entry := range entries {
+			if entry.IsDir() {
+				continue
+			}
+			outFile := filepath.Join(tmpDir, entry.Name())
+			doc := spdx22JSON.Document{}
+			data, err := os.ReadFile(outFile)
+			if err != nil {
+				panic(err)
+			}
+			json.Unmarshal(data, &doc)
+			merged.Files = append(merged.Files, doc.Files...)
+			merged.Packages = append(merged.Packages, doc.Packages...)
+			merged.Relationships = append(merged.Relationships, doc.Relationships...)
+		}
+		if err != nil {
+			panic(err)
+		}
+		scanResultsByTool["spdx_tool"] = merged
+	}()
+
+	// // // k8s BOM generator
+	go func() {
+		defer wg.Done()
+		k8BomOut, err := exec.Command("docker", "run", "-v", fullPath+":"+"/scan", K8BomImageURL, "generate", "/scan", "--format", "json").Output()
+		if err != nil {
+			panic(err)
+		}
+		k8Bom := spdx22JSON.Document{}
+		if err := json.Unmarshal(k8BomOut, &k8Bom); err != nil {
+			panic(err)
+		}
+		scanResultsByTool["k8_bom"] = k8Bom
+	}()
+
+	// // syft BOM generator
+	go func() {
+		defer wg.Done()
+		syftBomOut, err := exec.Command(
+			"docker", "run", "-v",
+			fullPath+":"+"/scan", SyftImageURL,
+			"packages",
+			"dir:/scan", "-o", "spdx-json").CombinedOutput()
+		if err != nil {
+			panic(err)
+		}
+		syftRes := spdx22JSON.Document{}
+		json.Unmarshal(syftBomOut, &syftRes)
+		scanResultsByTool["syft"] = syftRes
+	}()
+
+	fmt.Println("Triggered all scans")
+	wg.Wait()
+
+	data, err := json.Marshal(scanResultsByTool)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(out))
+	if err := os.WriteFile("merged_bom.json", data, 0666); err != nil {
+		panic(err)
+	}
+
 }
---
+++ mergers.go
@@ -0,0 +1,25 @@
+package main
+
+import spdx22JSON "sigs.k8s.io/bom/pkg/spdx/json/v2.2.2"
+
+func mergeCreationInfo(bomByTools map[string]spdx22JSON.Document) spdx22JSON.CreationInfo {
+	ret := spdx22JSON.CreationInfo{}
+	for _, bom := range bomByTools {
+		ret.Creators = append(ret.Creators, bom.CreationInfo.Creators...)
+		if ret.LicenseListVersion < ret.LicenseListVersion {
+			ret.LicenseListVersion = ret.LicenseListVersion
+		}
+	}
+	return ret
+}
+
+// func createPackageIndex(bomByTools map[string]spdx22JSON.Document) {
+// 	packagesByNameVersion := make(map[string][]spdx22JSON.Package)
+// 	for _, bom := range bomByTools{
+// 		for _, package := range bom.Packages {
+// 			packapackagesByNameVersion[
+// 				package
+// 			]
+// 		}
+// 	}
+// }
