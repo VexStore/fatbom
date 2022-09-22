a410a3914264708b7b3d461f0a2f07ec895c4491
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2022-09-22 13:49:51 +0530

    Update types and add goreleaser
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- types.go
+++ types.go
@@ -12,6 +12,12 @@ const (
 	MsBom
 )
 
+const (
+	SyftImageURL  string = "docker.io/anchore/syft"
+	K8BomImageURL string = "docker.io/sbs2001/k8s_bom:latest"
+	MSBomURL      string = "ghcr.io/sbs2001/ms_sbom:v0.0.3"
+	SpdxImageURL  string = "docker.io/spdx/spdx-sbom-generator"
+)
 
 type SpdxPackageExternalRef struct {
 	spdx22JSON.ExternalRef
